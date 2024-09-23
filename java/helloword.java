93ce303725b7aebe2372f6dc83d022d36d3b98c4
Doan Thanh Tung <tung200456789@gmail.com>
2024-09-23 14:23:00 +0700

    first commit

---
+++ java/helloword.java
@@ -0,0 +1,9 @@
+package hello.java;
+
+//Example 1: helloword.java
+//Text-printing program
+public class helloword {
+	public static void main(String args[]) {
+		System.out.println("Doan Thanh Tung - 5946\n" + "xin chao cac ban\n" + "hello \t world");
+	}// end of method main
+}
